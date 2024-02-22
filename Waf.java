7f8d7b5b7a9f8b93248255f381d379bcea7743ab
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-22 11:54:10 +0530

    fourth commit

--- Waf.java
+++ Waf.java
@@ -8,4 +8,10 @@ public class Waf
         int b = sc.nextInt();
         int height = sc.nextInt();
     }
+
+    public static int addition(int num1,int num2)
+    {
+        int sum = num1+num2;
+        return sum;
+    }
 }
